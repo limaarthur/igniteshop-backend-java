0d54a7b6afb4bdd1735ca83ccbc43a8afc5f2f1d
limaarthur <artglima.dev@gmail.com>
2025-02-27 11:37:52 -0300

    first commit

---
+++ src/main/java/com/ignite/igniteshop/IgniteshopApplication.java
@@ -0,0 +1,13 @@
+package com.ignite.igniteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignite/igniteshop/IgniteshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignite.igniteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
