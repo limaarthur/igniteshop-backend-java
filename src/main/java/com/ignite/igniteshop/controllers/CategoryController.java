8f3105a50d2954f8b5c462846b3a0354a5485d84
limaarthur <artglima.dev@gmail.com>
2025-02-28 09:38:33 -0300

    feat: get all categories in controller

---
+++ src/main/java/com/ignite/igniteshop/controllers/CategoryController.java
@@ -0,0 +1,27 @@
+package com.ignite.igniteshop.controllers;
+
+import com.ignite.igniteshop.dtos.CategoryDTO;
+import com.ignite.igniteshop.services.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> categoryDTO = categoryService.findAll();
+        return ResponseEntity.ok(categoryDTO);
+    }
+}
