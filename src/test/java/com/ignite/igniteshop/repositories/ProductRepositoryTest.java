8d4d764d2298e1f58c7988eb21d430747f85c83a
limaarthur <artglima.dev@gmail.com>
2025-03-07 09:22:38 -0300

    feat: tests - test save with null id

--- src/test/java/com/ignite/igniteshop/repositories/ProductRepositoryTest.java
+++ src/test/java/com/ignite/igniteshop/repositories/ProductRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.ignite.igniteshop.repositories;
 
+import com.ignite.igniteshop.entities.Product;
 import com.ignite.igniteshop.services.ProductService;
 import com.ignite.igniteshop.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,11 +23,31 @@ class ProductRepositoryTest {
 
     private long existsId;
     private long nonExistentId;
+    private long countTotalProducts;
 
     @BeforeEach
     public void setUp() {
         existsId = 10L;
-        nonExistentId = 1L;
+        nonExistentId = 1000L;
+        countTotalProducts = 35L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+        Product product = new Product(null,
+                "Phone",
+                "Good Phone",
+                800.0,
+                "https://img.com/img.jpg",
+                Instant.parse("2020-10-20T03:00:00Z"
+                ));
+        product.setId(null);
+
+        Product savedProduct = productRepository.save(product);
+
+        assertNotNull(savedProduct.getId());
+
+        assertEquals(countTotalProducts + 1, product.getId());
     }
 
     @Test
