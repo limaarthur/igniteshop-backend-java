1f72c6b016bf2e9f20af12326053fe21ada4852b
limaarthur <artglima.dev@gmail.com>
2025-03-10 11:32:48 -0300

    feat: tests - testing findbyid and update existing and non-existing id with exceptions

--- src/test/java/com/ignite/igniteshop/services/ProductServiceTest.java
+++ src/test/java/com/ignite/igniteshop/services/ProductServiceTest.java
@@ -1,11 +1,14 @@
 package com.ignite.igniteshop.services;
 
 import com.ignite.igniteshop.dtos.ProductDTO;
+import com.ignite.igniteshop.entities.Category;
 import com.ignite.igniteshop.entities.Product;
+import com.ignite.igniteshop.repositories.CategoryRepository;
 import com.ignite.igniteshop.repositories.ProductRepository;
 import com.ignite.igniteshop.services.exceptions.DataBaseException;
 import com.ignite.igniteshop.services.exceptions.ResourceNotFoundException;
 import com.ignite.igniteshop.tests.Factory;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,11 +38,16 @@ class ProductServiceTest {
     @Mock
     private ProductRepository productRepository;
 
+    @Mock
+    private CategoryRepository categoryRepository;
+
     private long existingId;
     private long nonExistingId;
     private long dependentId;
     private Product product;
+    private Category category;
     private PageImpl<Product> page;
+    private ProductDTO productDTO;
 
     @BeforeEach
     void setUp() {
@@ -47,6 +55,8 @@ class ProductServiceTest {
         nonExistingId = 1000L;
         dependentId = 3L;
         product = Factory.createdProduct();
+        productDTO = Factory.createdProductDTO();
+        category = Factory.createCategory();
         page = new PageImpl<>(List.of(product));
 
         Mockito.lenient().when(productRepository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
@@ -56,9 +66,37 @@ class ProductServiceTest {
         Mockito.lenient().when(productRepository.findById(existingId)).thenReturn(Optional.of(product));
         Mockito.lenient().when(productRepository.findById(nonExistingId)).thenReturn(Optional.empty());
 
+        Mockito.lenient().when(productRepository.getOne(existingId)).thenReturn(product);
+        Mockito.lenient().when(productRepository.getOne(nonExistingId)).thenThrow(EntityNotFoundException.class);
+
+        Mockito.lenient().when(categoryRepository.getOne(existingId)).thenReturn(category);
+        Mockito.lenient().when(categoryRepository.getOne(nonExistingId)).thenThrow(EntityNotFoundException.class);
+
+
         Mockito.lenient().doNothing().when(productRepository).deleteById(existingId);
         Mockito.lenient().doThrow(ResourceNotFoundException.class).when(productRepository).deleteById(nonExistingId);
         Mockito.lenient().doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenExisting() {
+
+        productDTO = Factory.createdProductDTO();
+
+        ProductDTO result = productService.update(existingId, productDTO);
+
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+
+         productDTO = Factory.createdProductDTO();
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+
+            productService.update(nonExistingId, productDTO);
+        });
     }
 
     @Test
