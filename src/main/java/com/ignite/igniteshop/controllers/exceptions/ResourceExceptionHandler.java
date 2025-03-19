5021626115b58088721bf66557bfcd12d497931d
limaarthur <artglima.dev@gmail.com>
2025-03-19 18:19:11 -0300

    feat: custom validation response

--- src/main/java/com/ignite/igniteshop/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/ignite/igniteshop/controllers/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import com.ignite.igniteshop.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,14 +40,20 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardError> validationException(MethodArgumentNotValidException e, HttpServletRequest request) {
+    public ResponseEntity<ValidationError> validationException(MethodArgumentNotValidException e, HttpServletRequest request) {
         HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
-        StandardError err = new StandardError();
+        ValidationError err = new ValidationError();
         err.setTimestamp(Instant.now());
         err.setStatus(httpStatus.value());
         err.setError("Validation exception");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
         return ResponseEntity.status(httpStatus).body(err);
+
     }
 }
