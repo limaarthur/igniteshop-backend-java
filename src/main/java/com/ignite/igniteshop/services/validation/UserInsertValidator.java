2ebc08a5ff1d14bc5948754c1814492c5f51fe09
limaarthur <artglima.dev@gmail.com>
2025-03-20 09:36:28 -0300

    feat: creating the user email search method and testing the validation

--- src/main/java/com/ignite/igniteshop/services/validation/UserInsertValidator.java
+++ src/main/java/com/ignite/igniteshop/services/validation/UserInsertValidator.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.ignite.igniteshop.dtos.UserInsertDTO;
+import com.ignite.igniteshop.entities.User;
+import com.ignite.igniteshop.repositories.UserRepository;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
@@ -11,16 +13,25 @@ import com.ignite.igniteshop.controllers.exceptions.FieldMessage;
 
 public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
 
+    private UserRepository userRepository;
+
+    public UserInsertValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public void initialize(UserInsertValid ann) {
     }
 
     @Override
-    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+    public boolean isValid(UserInsertDTO userInsertDTO, ConstraintValidatorContext context) {
 
         List<FieldMessage> list = new ArrayList<>();
 
-        // Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+        User user = userRepository.findByEmail(userInsertDTO.getEmail());
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe"));
+        }
 
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
