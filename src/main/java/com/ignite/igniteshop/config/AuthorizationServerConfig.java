2512a4f07b123ba849fbda9d2671e9623a220f48
limaarthur <artglima.dev@gmail.com>
2025-03-26 18:25:06 -0300

    feat: configuring the WebSecurityConfig class

---
+++ src/main/java/com/ignite/igniteshop/config/AuthorizationServerConfig.java
@@ -0,0 +1,4 @@
+package com.ignite.igniteshop.config;
+
+public class AuthorizationServerConfig {
+}
---
+++ src/main/java/com/ignite/igniteshop/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.ignite.igniteshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebSecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()  // Ignora segurança para /actuator
+                        .anyRequest().authenticated())  // Exige autenticação para as demais requisições
+                .csrf(csrf -> csrf.disable())  // Desabilita CSRF (necessário em algumas APIs)
+                .httpBasic(Customizer.withDefaults());  // Usando a configuração padrão de autenticação básica HTTP
+
+        return http.build();
+    }
+}
