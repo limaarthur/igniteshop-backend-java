dce66d37f305cb233c3eea3c39d87e7df98dd0c7
limaarthur <artglima.dev@gmail.com>
2025-03-26 18:25:06 -0300

    feat: create userdetail, userdatailsprojection and userdetailsservice

--- src/main/java/com/ignite/igniteshop/repositories/UserRepository.java
+++ src/main/java/com/ignite/igniteshop/repositories/UserRepository.java
@@ -1,9 +1,24 @@
 package com.ignite.igniteshop.repositories;
 
 import com.ignite.igniteshop.entities.User;
+import com.ignite.igniteshop.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+	User findByEmail(String email);
+
+    @Query(nativeQuery = true, value = """
+				SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
+				FROM tb_user
+				INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
+				INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+				WHERE tb_user.email = :email
+			""")
+    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
 }
